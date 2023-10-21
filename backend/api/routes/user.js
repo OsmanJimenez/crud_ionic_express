eba24ceea4533ae432d8352eafceff14f430bc65
OsmanJimenez <osmanjimenez.dev@gmail.com>
2023-10-21 13:13:00 -0500

    Chore: adjust comments in code #4

--- backend/api/routes/user.js
+++ backend/api/routes/user.js
@@ -1,89 +1,98 @@
 const express = require('express');
 const router = express.Router();
+const mysqlConnection = require('../connection/connection');
+const jwt = require('jsonwebtoken');
 
-const mysqlConnection = require('../connection/connection')
-const jwt = require('jsonwebtoken')
-
+// Obtener todos los usuarios
 router.get('/', (req, res) => {
-    mysqlConnection.query('SELECT * FROM users', (err, rows, fields) => {
-        if(!err){
-            res.json(rows);
-        }else{
-            console.log(err)
-        }
-    })
-});
-
-router.post('/login', (req, res) => {
-const {email, password} = req.body;
-mysqlConnection.query('SELECT * FROM users WHERE email = ? AND password = ?', [email, password], (err, rows, fields) => {
-    if(!err){
-        if(rows.length > 0){
-            const user = rows[0];
-            const token = jwt.sign({id: user.id, email: user.email}, 'secret-token', {expiresIn: '1h'})
-            res.json({token: token});
-        }else{
-            res.status(400).send('Usuario o clave invalida.')
-        }
-    }else{
-        console.log(err)
-    }
-}
-)
-})
-
-router.post('/', verifyToken, (req, res) => {
-  const { email, password, role } = req.body;
-  const query = 'INSERT INTO users (email, password, role) VALUES (?, ?, ?)';
-  mysqlConnection.query(query, [email, password, role], (err, result) => {
-    if (!err) {
-      res.json({ message: 'Usuario agregado correctamente' });
-    } else {
+  mysqlConnection.query('SELECT * FROM users', (err, rows) => {
+    if (err) {
       console.log(err);
-      res.status(500).json({ error: 'Error al agregar el usuario' });
+      return res.status(500).json({ error: 'Error al obtener los usuarios' });
     }
+    res.json(rows);
   });
 });
 
+// Autenticación de usuarios
+router.post('/login', (req, res) => {
+  const { email, password } = req.body;
+  mysqlConnection.query(
+    'SELECT * FROM users WHERE email = ? AND password = ?',
+    [email, password],
+    (err, rows) => {
+      if (err) {
+        console.log(err);
+        return res.status(500).json({ error: 'Error al autenticar el usuario' });
+      }
+      if (rows.length > 0) {
+        const user = rows[0];
+        const token = jwt.sign({ id: user.id, email: user.email }, 'secret-token', {
+          expiresIn: '1h',
+        });
+        res.json({ token });
+      } else {
+        res.status(400).send('Usuario o clave inválida.');
+      }
+    }
+  );
+});
+
+// Agregar un nuevo usuario
+router.post('/', verifyToken, (req, res) => {
+  const { email, password, role } = req.body;
+  const query = 'INSERT INTO users (email, password, role) VALUES (?, ?, ?)';
+  mysqlConnection.query(query, [email, password, role], (err) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).json({ error: 'Error al agregar el usuario' });
+    }
+    res.json({ message: 'Usuario agregado correctamente' });
+  });
+});
+
+// Actualizar un usuario existente
 router.put('/:id', (req, res) => {
   const userId = req.params.id;
   const { email, password, role } = req.body;
   const query = 'UPDATE users SET email = ?, password = ?, role = ? WHERE id = ?';
-  mysqlConnection.query(query, [email, password, role, userId], (err, result) => {
-    if (!err) {
-      res.json({ message: 'Usuario actualizado correctamente' });
-    } else {
+  mysqlConnection.query(query, [email, password, role, userId], (err) => {
+    if (err) {
       console.log(err);
-      res.status(500).json({ error: 'Error al actualizar el usuario' });
+      return res.status(500).json({ error: 'Error al actualizar el usuario' });
     }
+    res.json({ message: 'Usuario actualizado correctamente' });
   });
 });
 
+// Eliminar un usuario
 router.delete('/:id', (req, res) => {
   const userId = req.params.id;
   const query = 'DELETE FROM users WHERE id = ?';
-  mysqlConnection.query(query, [userId], (err, result) => {
-    if (!err) {
-      res.json({ message: 'Usuario eliminado correctamente' });
-    } else {
+  mysqlConnection.query(query, [userId], (err) => {
+    if (err) {
       console.log(err);
-      res.status(500).json({ error: 'Error al eliminar el usuario' });
+      return res.status(500).json({ error: 'Error al eliminar el usuario' });
     }
+    res.json({ message: 'Usuario eliminado correctamente' });
   });
 });
 
-function verifyToken(req,res, next){
-  if(!req.headers.authorization) return res.status(401).json('No autorizado');
-
-  const token = req.headers.authorization.substr(7);
-  if(token!==''){
-    const content = jwt.verify(token,'secret-token');
-    req.data = content;
-    next();
-  }else{
-    res.status(401).json('Token vacio');
+// Función de verificación de token para proteger rutas
+function verifyToken(req, res, next) {
+  const token = req.headers.authorization?.split(' ')[1];
+  if (!token) {
+    return res.status(401).json('No autorizado');
   }
 
+  try {
+    const content = jwt.verify(token, 'secret-token');
+    req.data = content;
+    next();
+  } catch (err) {
+    console.log(err);
+    res.status(401).json('Token inválido');
+  }
 }
 
 module.exports = router;
