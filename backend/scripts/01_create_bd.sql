aff23f297cce943708e590fe04e70969dfc68718
OsmanJimenez <osmanjimenez.dev@gmail.com>
2023-10-21 11:52:50 -0500

    Feat: initialize backend #2

--- backend/scripts/01_create_bd.sql
+++ backend/scripts/01_create_bd.sql
@@ -1,4 +1,1 @@
-IF NOT EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DB_Test')
-BEGIN
-    CREATE DATABASE db_test;
-END
+CREATE DATABASE IF NOT EXISTS db_test;
--- backend/scripts/02_create_table.sql
+++ backend/scripts/02_create_table.sql
@@ -1,11 +1,8 @@
-USE DB_Test
+USE DB_Test;
 
-IF NOT EXISTS (SELECT 1 FROM sys.tables WHERE name = 'Users')
-BEGIN
-    CREATE TABLE Users (
-        id INT IDENTITY(1,1) PRIMARY KEY,
-        email VARCHAR(255) NOT NULL UNIQUE,
-        password VARCHAR(255) NOT NULL,
-        role VARCHAR(50) NOT NULL
-    );
-END
+CREATE TABLE IF NOT EXISTS Users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(50) NOT NULL
+);
